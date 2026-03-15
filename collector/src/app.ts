5da0658523b938c5eb44edb30031d1a207b5917b
Abhaysoft-inc <av385735@gmail.com>
2026-03-15 12:44:26 +0530

    feat: wrote schema for user and projects

--- collector/src/app.ts
+++ collector/src/app.ts
@@ -1,3 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = Number(process.env.PORT) || 3000;
+
+
+app.listen(port, () => console.log("Server is listening on PORT: ", port));
+
+export default app;
