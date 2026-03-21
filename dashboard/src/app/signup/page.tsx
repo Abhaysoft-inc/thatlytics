06f518943ea1f2dabaff82b6e9301ef805be4e2f
Abhaysoft-inc <av385735@gmail.com>
2026-03-21 21:43:05 +0530

    feat: update dashboard with new UI components and styles
    - Added new dependencies for UI components and animations.
    - Updated global styles to include new color variables and dark mode support.
    - Refactored layout to utilize new font and utility functions.
    - Created new login and signup pages with forms.
    - Implemented reusable UI components for buttons, inputs, and labels.
    - Enhanced input fields with motion effects for better user experience.

---
+++ dashboard/src/app/signup/page.tsx
@@ -0,0 +1,15 @@
+import SignupFormDemo from '@/components/signup-form-demo'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+
+            <SignupFormDemo />
+
+
+        </div>
+    )
+}
+
+export default page
