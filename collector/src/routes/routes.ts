47a7b68b971b725c108a2357021503052fdb87f6
Abhaysoft-inc <av385735@gmail.com>
2026-04-05 23:05:49 +0530

    feat: implement authentication middleware for token verification

--- collector/src/routes/routes.ts
+++ collector/src/routes/routes.ts
@@ -1,9 +1,11 @@
 import controllerRoute from '../modules/collector/collector.routes.js'
+import authRoute from '../modules/auth/auth.routes.js'
 import { Router } from 'express'
 
 
 const routes = Router();
 
+routes.use('/auth', authRoute);
 routes.use('/collector', controllerRoute);
 
 export default routes;
